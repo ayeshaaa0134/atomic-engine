5d3a882d46071ee17d5fd594f6d5b5699137d73e
Atomic Tree Dev <dev@atomictree.com>
2025-12-31 07:55:58 -0800

    trying to fix errors

---
+++ build/CMakeFiles/example-code.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example-code.dir/D_/DSA-Project/examples/my_custom_code.cpp.obj"
+  "CMakeFiles/example-code.dir/D_/DSA-Project/examples/my_custom_code.cpp.obj.d"
+  "example-code.exe"
+  "example-code.exe.manifest"
+  "example-code.pdb"
+  "libexample-code.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example-code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(StressTest "D:/DSA-Project/build/atomic-tests.exe")
-set_tests_properties(StressTest PROPERTIES  _BACKTRACE_TRIPLES "D:/DSA-Project/backend/CMakeLists.txt;32;add_test;D:/DSA-Project/backend/CMakeLists.txt;0;")
+set_tests_properties(StressTest PROPERTIES  _BACKTRACE_TRIPLES "D:/DSA-Project/backend/CMakeLists.txt;37;add_test;D:/DSA-Project/backend/CMakeLists.txt;0;")
