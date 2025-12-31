eb05987bfafe75fccb374da7e8beb920358eea0c
Atomic Tree Dev <dev@atomictree.com>
2025-12-31 07:12:47 -0800

    feat: Enhance chat UI with auto-scrolling messages and dynamic input height.

---
+++ build/CMakeFiles/atomic-engine.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atomic-engine.dir/src/allocator.cpp.obj"
+  "CMakeFiles/atomic-engine.dir/src/allocator.cpp.obj.d"
+  "CMakeFiles/atomic-engine.dir/src/b_tree.cpp.obj"
+  "CMakeFiles/atomic-engine.dir/src/b_tree.cpp.obj.d"
+  "CMakeFiles/atomic-engine.dir/src/manager.cpp.obj"
+  "CMakeFiles/atomic-engine.dir/src/manager.cpp.obj.d"
+  "CMakeFiles/atomic-engine.dir/src/primitives.cpp.obj"
+  "CMakeFiles/atomic-engine.dir/src/primitives.cpp.obj.d"
+  "CMakeFiles/atomic-engine.dir/src/wort.cpp.obj"
+  "CMakeFiles/atomic-engine.dir/src/wort.cpp.obj.d"
+  "atomic-engine.exe"
+  "atomic-engine.exe.manifest"
+  "atomic-engine.pdb"
+  "libatomic-engine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/atomic-engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/atomic-tests.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/DSA-Project/backend/src/allocator.cpp" "CMakeFiles/atomic-tests.dir/src/allocator.cpp.obj" "gcc" "CMakeFiles/atomic-tests.dir/src/allocator.cpp.obj.d"
+  "D:/DSA-Project/backend/src/b_tree.cpp" "CMakeFiles/atomic-tests.dir/src/b_tree.cpp.obj" "gcc" "CMakeFiles/atomic-tests.dir/src/b_tree.cpp.obj.d"
+  "D:/DSA-Project/backend/src/manager.cpp" "CMakeFiles/atomic-tests.dir/src/manager.cpp.obj" "gcc" "CMakeFiles/atomic-tests.dir/src/manager.cpp.obj.d"
+  "D:/DSA-Project/backend/src/primitives.cpp" "CMakeFiles/atomic-tests.dir/src/primitives.cpp.obj" "gcc" "CMakeFiles/atomic-tests.dir/src/primitives.cpp.obj.d"
+  "D:/DSA-Project/backend/tests/stress_test.cpp" "CMakeFiles/atomic-tests.dir/tests/stress_test.cpp.obj" "gcc" "CMakeFiles/atomic-tests.dir/tests/stress_test.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
